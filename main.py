eb9dc5275d16a18b4d3d4e18b6037a197ab132d2
Zoe Oboler <101363536+ZoeO11@users.noreply.github.com>
2023-02-24 02:35:14 +0000

    New Update

--- main.py
+++ main.py
@@ -38,14 +38,44 @@ class Result:
     def __repr__(self):
         return('longest_size=%d left_size=%d right_size=%d is_entire_range=%s' %
               (self.longest_size, self.left_size, self.right_size, self.is_entire_range))
+
+def longest_run_recursive_help(mylist, key):
+  if len(mylist) == 1:
+    if mylist[0] == key:
+      return Result(1,1,1,True)
+    else:
+      return Result(0,0,0,False)
     
+  else:
+      r = mylist[(len(mylist)//2):]
+      l = mylist[:(len(mylist)//2)]
+      right = longest_run_recursive_help(r,key)
+      left = longest_run_recursive_help(l,key)
+
+      if right.is_entire_range:
+        if left.is_entire_range:
+          sum = right.longest_size + left.longest_size
+          return Result(sum,sum,sum,True)
+        else:
+          if right.longest_size >= (left.right_size + right.left_size):
+            sum = right.longest_size
+          else: sum = (left.right_size + right.left_size)
+        return Result(left.left_size, right.right_size + left.right_size, sum, False)
+      elif left.is_entire_range:
+        if left.longest_size < (right.left_size + left.longest_size):
+          sum = left.longest_size + right.left_size
+        else:
+          sum = left.longest_size
+        return Result(left.left_size + right.left_size,right.right_size, sum, False)
+      else:
+        if (left.right_size + right.left_size) < left.longest_size:
+          sum = left.longest_size
+        elif (left.right_size + right.left_size) < right.longest_size:
+          sum = right.longest_size
+        else:
+          sum = right.left_size + left.right_size
+        return Result(left.left_size, right.right_size, sum, False)
+  
     
 def longest_run_recursive(mylist, key):
-    ### TODO
-    pass
-
-## Feel free to add your own tests here.
-def test_longest_run():
-    assert longest_run([2,12,12,8,12,12,12,0,12,1], 12) == 3
-
-test_longest_run()
+  return longest_run_recursive_help(mylist,key).longest_size
